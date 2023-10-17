d7fd190b89b75f27ad8ad6a5a057264ce33fd254
TaisYuri <tais.yuri@jfpart.com.br>
2023-10-16 23:22:08 -0300

    image control for skills

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,5 +1,6 @@
-
+'use client'
 import Image from 'next/image'
+import { useCallback, useState } from 'react';
 
 export interface SkillsProps {
   icon: string;
@@ -12,14 +13,29 @@ export interface SkillsProps {
 }
 
 function Skills({ description, icon,skills }: SkillsProps) {
+  
+  const [tagId, setTagId] = useState(0)
+
+  const renderImage = useCallback((id: number)=> {
+    const image: {[key:number]: string} = {
+      1 : "/icon-figma.svg",
+      2: "/icon-css3.svg",
+      3: "/icon-python.svg",
+      4: "/design-responsivo.png",
+      5: "/design-responsivo.png",
+      6:"/icon-figma.svg"
+    }
+    return image[id]
+  },[])
+
   return (
     <div
       className="flex flex-1"
     >
       <div className='bg-red-300'>
         <Image
-        className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark m-auto"
-        src={icon}
+        className="relative m-auto"
+        src={renderImage(tagId)}
         alt="img"
         width={160}
         height={37}
@@ -30,9 +46,9 @@ function Skills({ description, icon,skills }: SkillsProps) {
         </p>
       </div>
       <div className='bg-yellow-300'>
-        <div className='grid text-center gap-5 md:grid-cols-2 '>
+        <div className='grid text-center gap-3 md:grid-cols-2 ml-7'>
           {skills.map( item => 
-          <div key={item.id} className='border border-transparent mx-3 px-5 py-4 bg-[#FA7436]'>{item.skill}</div>
+          <div key={item.id} onMouseOver={() => setTagId(item.id)} className='border border-transparent rounded-md mx-3 px-5 py-4 bg-[#FA7436] text-white'>{item.skill}</div>
             )}
          
         </div>
--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,10 +1,10 @@
 export const skills = [
-    {id: 1, skill: "Autodidata"},
-    {id: 2, skill: "Comunicativa"},
-    {id: 3, skill: "Empenhada"},
-    {id: 4, skill: "Analista"},
-    {id: 5, skill: "Vestindo a camisa"},
-    {id: 6, skill: "Trabalho em grupo"},
-    {id: 7, skill: "Trabalho individualmente"},
-    {id: 8, skill: "Analista"},
+    {id: 1, skill: "Autodidata", img:"/icon-figma.svg"},
+    {id: 2, skill: "Comunicativa", img:"/icon-css3.svg"},
+    {id: 3, skill: "Empenhada", img:"/icon-python.svg"},
+    {id: 4, skill: "Analista", img:"/icon-figma.svg"},
+    {id: 5, skill: "Vestindo a camisa", img:"/design-responsivo.png"},
+    {id: 6, skill: "Trabalho em grupo", img:"/icon-figma.svg"},
+    {id: 7, skill: "Trabalho individualmente", img:"/icon-figma.svg"},
+    {id: 8, skill: "Analista", img:"/icon-figma.svg"},
 ]
